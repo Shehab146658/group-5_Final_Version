f1af67d4c189101f62793f917377c4bafee9de4c
meransharawy97 <37513129+merannnnn@users.noreply.github.com>
2018-12-08 14:19:53 +0200

    test case

--- gameOnlineStoreSystem/src/testcase/TestCase.java
+++ gameOnlineStoreSystem/src/testcase/TestCase.java
@@ -6,31 +6,38 @@ package testcase;
  */
 import static org.junit.Assert.*;  
 import gameonlinestoresystem.*;  
+import java.util.ArrayList;
 import mongoDB.*;
 import org.junit.*;  
 
 public class TestCase {
-    @Test
-    public void loginTESTING(String username,String password)
-    {
-        AccountManager instance = new AccountManager();
-        assertEquals(username,instance.username);
-        assertEquals(password,instance.password);
-    }
-    
-    @Test
+        @Test
     public void getCategoryByNameTESTING(Category c)
     {
         DB d1= new DB();
         assertEquals(c.getCategoryName(),d1.getCategoryByName(c.getCategoryName()));
                                
+    } 
+         @Test
+    public void updateTicketTESTING(Ticket c,String id)
+    {
+        DB db= new DB();
+        assertEquals(c,db.getTicketByID(id));
+    }
+        @Test
+    public void deleteshoppingCartTESTING(shoppingCart s)
+    {
+        DB db= new DB();
+        db.deleteshoppingCart(s);
+        assertNull(s);
     }
     
-    @Test 
-    public void updateCategoryTESTING (Category c1)
-    {
-        DB d2= new DB();
-        //assertEquals(c1, d2.updateCategory(c1));
-    }
+   @Test
+   public void insertItemTESTING(Item i)
+   {
+       DB db= new DB();
+       db.insertItem(i);
+       assertNotNull(i);
+   }
     
 }
